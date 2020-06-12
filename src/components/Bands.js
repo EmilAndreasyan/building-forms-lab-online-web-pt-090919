95eab817bef3685f549d7e541bb7e68e5e0c8b32
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 04:47:55 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Bands = props => {
-  const bands = props.bands.map((band, index) => )
+  const bands = props.bands.map((band, index) => {
+    return <li key={index} ></li>
+  })
 }
 export default Bands;
